7c2e428ca94c09a94edf5f38450016cc56ac8f22
Russell Toris <russell.toris@gmail.com>
2014-11-26 15:11:15 -0500

    learner node script added

---
+++ jinteractiveworld/src/main/java/edu/wpi/rail/jinteractiveworld/ros/InteractiveWorldLearner.java
@@ -0,0 +1,10 @@
+package edu.wpi.rail.jinteractiveworld.ros;
+
+public class InteractiveWorldLearner {
+
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("== THIS IS A TEST ==");
+		System.out.println();
+	}
+}
