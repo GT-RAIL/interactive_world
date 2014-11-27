f2d362c7a2461bd5f29ef0b1f8c0d7fe2fb0bbd9
Russell Toris <russell.toris@gmail.com>
2014-11-27 13:58:24 -0500

    0.2.0 jrosbridge used

--- jinteractiveworld/src/main/java/edu/wpi/rail/jinteractiveworld/ros/InteractiveWorldLearner.java
+++ jinteractiveworld/src/main/java/edu/wpi/rail/jinteractiveworld/ros/InteractiveWorldLearner.java
@@ -1,10 +1,38 @@
 package edu.wpi.rail.jinteractiveworld.ros;
 
+import edu.wpi.rail.jrosbridge.Ros;
+import edu.wpi.rail.jrosbridge.Service;
+
 public class InteractiveWorldLearner {
 
-	public static void main(String[] args) {
-		System.out.println();
-		System.out.println("== THIS IS A TEST ==");
-		System.out.println();
+	public static final String NODE_NAME = "interactive_world_learner";
+
+	public static final String LEARN_HYPOTHESES_SERVICE_NAME = "/" + InteractiveWorldLearner.NODE_NAME + "/learn_hypotheses";
+
+	private Ros ros;
+
+	public InteractiveWorldLearner() {
+		ros = new Ros();
+		System.out.print("Attempting connection to rosbridge...");
+		if (!ros.connect()) {
+			System.out.println("No connection made. Verify rosbridge_websocket is running.");
+			System.exit(-1);
+		}
+		System.out.println(" success!");
+
+		// setup the services
+		Service s = new Service(ros, InteractiveWorldLearner.LEARN_HYPOTHESES_SERVICE_NAME, "");
+	}
+
+	public void spin() throws InterruptedException {
+		while (true) {
+			Thread.sleep(1000);
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		// create the learner object
+		InteractiveWorldLearner l = new InteractiveWorldLearner();
+		l.spin();
 	}
 }
